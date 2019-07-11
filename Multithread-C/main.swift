4beda098f30783e9e46e4f00d8bfccf3e26e8dc0
Artem <inste@icloud.com>
2019-07-11 20:54:48 +0300

    Initial Commit

---
+++ Multithread-C/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Multithread-C
+//
+//  Created by Артём Кармазь on 7/11/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
