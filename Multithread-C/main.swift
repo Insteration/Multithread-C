1f97452ca5b161a300ff63b13297ad0bdf905eac
Artem <inste@icloud.com>
2019-07-12 22:21:38 +0300

    FC

--- Multithread-C/main.swift
+++ Multithread-C/main.swift
@@ -8,5 +8,13 @@
 
 import Foundation
 
-print("Hello, World!")
+var thread = pthread_t(bitPattern: 0)
+var attribute = pthread_attr_t()
+
+pthread_attr_init(&attribute)
+
+pthread_create(&thread, &attribute, { (pointer) -> UnsafeMutableRawPointer? in
+    print("ok")
+    return nil
+}, nil)
 
