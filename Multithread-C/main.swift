c8c22a0a57bba55147b27d488266b8ab6cef30b2
Artem <inste@icloud.com>
2019-07-13 22:07:07 +0300

    nc2

--- Multithread-C/main.swift
+++ Multithread-C/main.swift
@@ -18,3 +18,22 @@ pthread_create(&thread, &attribute, { (pointer) -> UnsafeMutableRawPointer? in
     return nil
 }, nil)
 
+//var thread = pthread_t(bitPattern: 0)
+//var attribute = pthread_attr_t()
+//
+//pthread_attr_init(&attribute)
+//
+//pthread_create(&thread, &attribute, { (pointer) -> UnsafeMutableRawPointer? in
+//    print("ok")
+//    return nil
+//}, nil)
+//
+//var thread = pthread_t(bitPattern: 0)
+//var attribute = pthread_attr_t()
+//
+//pthread_attr_init(&attribute)
+//
+//pthread_create(&thread, &attribute, { (pointer) -> UnsafeMutableRawPointer? in
+//    print("ok")
+//    return nil
+//}, nil)
